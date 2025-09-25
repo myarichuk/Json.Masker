c27130f7d267ba03d8fba697c3812149451c201b
Michael Yarichuk <michael.yarichuk@gmail.com>
2025-09-25 18:51:43 +0300

    chore: started implementing Newtonsoft provider

---
+++ src/Json.Masker.Newtonsoft/MaskingContractResolver.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Json.Masker.Abstract;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Json.Masker.Newtonsoft;
+
+public sealed class MaskingContractResolver : DefaultContractResolver
+{
+    private readonly IMaskingService _maskingService;
+
+    public MaskingContractResolver(IMaskingService maskingService)
+    {
+        _maskingService = maskingService;
+    }
+
+    protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+    {
+        var prop = base.CreateProperty(member, memberSerialization);
+
+        var sensitiveAttr = member.GetCustomAttribute<SensitiveAttribute>();
+        if (sensitiveAttr is null)
+        {
+            return prop;
+        }
+
+        var inner = prop.ValueProvider;
+        
+        if (inner != null) 
+        {
+            prop.ValueProvider = new MaskingValueProvider(inner, _maskingService, sensitiveAttr);
+        }
+
+        return prop;
+    }
+}
