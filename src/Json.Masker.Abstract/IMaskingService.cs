4fa77b623edde9cdb1884eef7786c4ff0f157a95
Michael Yarichuk <michaely@bindecy.com>
2025-09-25 09:14:25 +0300

    feat(code):start writing shared lib

---
+++ src/Json.Masker.Abstract/IMaskingService.cs
@@ -0,0 +1,6 @@
+﻿namespace Json.Masker.Abstract;
+
+public interface IMaskingService
+{
+    string Mask(object? value, string strategy, MaskingContext ctx);
+}
---
+++ src/Json.Masker.Abstract/SensitiveAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Json.Masker.Abstract;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public sealed class SensitiveAttribute : Attribute
+{
+    public string Strategy { get; }
+    public SensitiveAttribute(string strategy = "default") => Strategy = strategy;
+}
