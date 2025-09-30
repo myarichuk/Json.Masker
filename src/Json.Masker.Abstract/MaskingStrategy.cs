128e070fe6c94dc3676be3e6966ce704f162d73b
Michael Yarichuk <michaely@bindecy.com>
2025-09-30 10:24:32 +0300

    chore: add basic plumbing for the types

---
+++ src/Json.Masker.Abstract/MaskingStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace Json.Masker.Abstract;
+
+public enum MaskingStrategy: byte
+{
+    Default = 0,
+    Creditcard,
+    Ssn
+}
---
+++ src/Json.Masker.Newtonsoft/NewtonsoftJsonMaskingConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using Json.Masker.Abstract;
+using Newtonsoft.Json;
+
+namespace Json.Masker.Newtonsoft;
+
+internal class NewtonsoftJsonMaskingConfigurator(IMaskingService maskingService) : IJsonMaskingConfigurator
+{
+    public void Configure(object settings)
+    {
+        if (settings is not JsonSerializerSettings opts)
+        {
+            return;
+        }
+
+        opts.ContractResolver = new MaskingContractResolver(maskingService);
+    }
+}
