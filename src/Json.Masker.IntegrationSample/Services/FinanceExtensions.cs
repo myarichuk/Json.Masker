dfb359a6f72cbd0489281e1ded08ceee3a54a8e4
Michael Yarichuk <michaely@bindecy.com>
2025-10-11 13:59:32 +0300

    chore: adjust and fix the sample app

---
+++ src/Json.Masker.IntegrationSample/Services/FinanceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Bogus;
+
+namespace Json.Masker.IntegrationSample.Services;
+
+public static class FinanceExtensions
+{
+    private static readonly string[] Issuers = { "Visa", "MasterCard", "American Express", "Discover" };
+
+    public static string CreditCardIssuer(this Faker faker)
+        => faker.PickRandom(Issuers);
+}
