51b6dc61e798916fe774f2b35a150db404acc04b
Michael Yarichuk <michael.yarichuk@gmail.com>
2025-10-11 10:03:52 +0300

    feat: convert integration sample to api with fake data

---
+++ src/Json.Masker.IntegrationSample/Models/CustomerStatus.cs
@@ -0,0 +1,9 @@
+namespace Json.Masker.IntegrationSample.Models;
+
+public enum CustomerStatus
+{
+    Prospect,
+    Active,
+    Suspended,
+    Closed
+}
---
+++ src/Json.Masker.IntegrationSample/Models/OrderStatus.cs
@@ -0,0 +1,12 @@
+namespace Json.Masker.IntegrationSample.Models;
+
+public enum OrderStatus
+{
+    Draft,
+    PendingPayment,
+    Paid,
+    Processing,
+    Shipped,
+    Fulfilled,
+    Cancelled
+}
