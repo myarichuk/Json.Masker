f4f293acdb235ae06a02c7019502a37838bb23c1
Michael Yarichuk <michael.yarichuk@gmail.com>
2025-10-11 10:09:02 +0300

    feat: mark sample business data as sensitive

--- src/Json.Masker.IntegrationSample/Models/Customer.cs
+++ src/Json.Masker.IntegrationSample/Models/Customer.cs
@@ -1,15 +1,27 @@
 using System.Linq;
+using Json.Masker.Abstract;
 
 namespace Json.Masker.IntegrationSample.Models;
 
 public class Customer
 {
     public Guid Id { get; set; }
+    [Sensitive]
     public string FirstName { get; set; } = string.Empty;
+
+    [Sensitive]
     public string LastName { get; set; } = string.Empty;
+
+    [Sensitive]
     public string Email { get; set; } = string.Empty;
+
+    [Sensitive]
     public string PhoneNumber { get; set; } = string.Empty;
+
+    [Sensitive]
     public Address BillingAddress { get; set; } = new();
+
+    [Sensitive]
     public Address ShippingAddress { get; set; } = new();
     public DateTimeOffset RegisteredAt { get; set; }
     public CustomerStatus Status { get; set; }
--- src/Json.Masker.IntegrationSample/Models/PaymentDetail.cs
+++ src/Json.Masker.IntegrationSample/Models/PaymentDetail.cs
@@ -1,12 +1,21 @@
+using Json.Masker.Abstract;
+
 namespace Json.Masker.IntegrationSample.Models;
 
 public class PaymentDetail
 {
     public string Method { get; set; } = string.Empty;
+
+    [Sensitive]
     public string CardIssuer { get; set; } = string.Empty;
+
+    [Sensitive]
     public string CardLast4 { get; set; } = string.Empty;
+
+    [Sensitive]
     public string TransactionReference { get; set; } = string.Empty;
     public DateTimeOffset? PaidAt { get; set; }
     public bool Refunded { get; set; }
+    [Sensitive]
     public string CustomerEmail { get; set; } = string.Empty;
 }
